8c9aaf0f8a7c9e43cacc5a78f704dab4e6e51e21
Laurens Wissels <laurenswissels@MacBook-Pro-van-Laurens.local>
2020-11-01 16:31:50 +0100

    init

---
+++ contracts/Name_list.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+
+contract Name_list {
+
+    mapping(string => uint) public names;
+    uint private last_id = 0;
+
+    function getID(string memory name )public view returns(uint ){
+        return names[name];
+    }
+
+    function addName(string memory name)public payable{
+        names[name] = last_id;
+        last_id += 1;
+    }
+
+}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Name_list = artifacts.require("Name_list");
+
+module.exports = function (deployer) {
+  deployer.deploy(Name_list);
+};
